2a94df54b9c94b95f6ebbfc29a7c59b83ad7e69d
Edwin Mercedes <81087505+frostyFozz@users.noreply.github.com>
2022-07-24 12:31:03 -0400

    actualizacion

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
+// estas son variables en short format
+
 package main
 
 import "fmt"
 
 func main() {
 
-	var dog string = "perrito"
-	fmt.Println(dog)
+	var dog, cat = "perrito", "\ngatito"
+	var num1 = 2
+	var num2 = 5.3
+	fmt.Println(dog, cat)
+	fmt.Println(num1)
+	fmt.Println(num2)
+
 }
