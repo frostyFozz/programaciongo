c3e7f37c267d3e9b1f2675d286ea89ed3505a148
Edwin Mercedes <81087505+frostyFozz@users.noreply.github.com>
2022-07-24 12:43:35 -0400

    Add files via upload

---
+++ .github/workflows/comentmMain.go
@@ -0,0 +1,16 @@
+// esta es la forma correcta de comentar en go
+// siempre se deben utilizar doble barra para hacer un buen uso del comentario
+package main
+
+// Test es una variable de pruebas
+var Test = 123
+
+// esta es una constante en go
+const Pi = 3.14
+
+/* func main() {
+
+
+
+}
+*/
